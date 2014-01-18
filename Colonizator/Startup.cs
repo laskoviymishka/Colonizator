aa354ecc76a680a6858d4a33b9a700a4176e4f7d
epam-mogilev <epam.mogilev@mail.ru>
2014-01-18 09:52:05 +0300

    initial

---
+++ Colonizator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Colonizator.Startup))]
+namespace Colonizator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
