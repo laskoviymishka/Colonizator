bd3472adaae2faf21831e710bdbd04b4b1945b03
epam-mogilev <epam.mogilev@mail.ru>
2014-01-18 14:53:23 +0300

    syncing

--- Colonizator/Controllers/GameController.cs
+++ Colonizator/Controllers/GameController.cs
@@ -6,11 +6,13 @@ using System.Web.Mvc;
 
 namespace Colonizator.Controllers
 {
-    public class HomeController : Controller
+    public class GameController : Controller
     {
+        //
+        // GET: /Game/
         public ActionResult Index()
         {
             return View();
         }
-    }
+	}
 }
