fc489de6c19bbb115d439aebfb9dd11bcce96f5d
ay-temp-5074b31d-6321 <c4580279@drdrb.com>
2014-01-18 13:19:33 +0300

    base classes

---
+++ GameLogic/Game/ResourceType.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLogic.Game
+{
+	public enum ResourceType
+	{
+		/// <summary>
+		/// Лес == Дерево
+		/// </summary>
+ 		Wood = 0, 
+
+		/// <summary>
+		/// Пастбище  ==  Шерсть
+		/// </summary>
+		Wool = 1, 
+
+		/// <summary>
+		/// Пашни  == Зерно
+		/// </summary>
+		Corn = 2,
+
+		/// <summary>
+		/// Холмы  ==  Глина
+		/// </summary>
+		Soil = 3,
+
+		/// <summary>
+		/// Горы  ==  Руда
+		/// </summary>
+		Minerals = 4
+	}
+}
--- GameLogic/Market/OrderType.cs
+++ GameLogic/Market/OrderType.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GameLogic.Game
+namespace GameLogic.Market
 {
-    class Order
+    public enum OrderType
     {
+        Buy = 0,
+        Sell = 1
     }
 }
