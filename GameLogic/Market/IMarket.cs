60e5c72e067a8dafb6043fb11966c117eceebe18
laskoviymishka <laskoviymishka@gmail.com>
2014-01-19 15:27:23 +0300

    initial marlket

--- GameLogic/Market/IMarket.cs
+++ GameLogic/Market/IMarket.cs
@@ -8,15 +8,23 @@ using GameLogic.Game;
 
 namespace GameLogic.Market
 {
-	public interface IMarket
-	{
-		IEnumerable<Order> GetOrders();
-		IEnumerable<Order> GetOrders(string playerId);
+    public interface IMarket
+    {
+        IEnumerable<Order> GetOrders();
+        IEnumerable<Order> GetOrders(string playerId);
 
-		bool PlaceOrder(Order order);
-		bool AcceptOder(Player acceptedBy, Guid orderId, Player orderOwner);
+        void BuildCity(Player player);
 
-		void SyncOrders(IEnumerable<Order> orders);
-		void ScavengeOrders(IEnumerable<Player> players);
-	}
+        void UpgardeCity(Player player);
+
+        void BuildRoad(Player player);
+
+        void RobberTime(IEnumerable<Player> players);
+
+        bool PlaceOrder(Order order);
+        bool AcceptOder(Player acceptedBy, Guid orderId, Player orderOwner);
+
+        void SyncOrders(IEnumerable<Order> orders);
+        void ScavengeOrders(IEnumerable<Player> players);
+    }
 }
