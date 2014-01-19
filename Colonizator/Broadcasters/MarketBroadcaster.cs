d21b89f42559d97566f84373077ed4cc9dc1dbf0
laskoviymishka <laskoviymishka@gmail.com>
2014-01-19 16:03:51 +0300

    market broadcaster init

--- Colonizator/Broadcasters/MarketBroadcaster.cs
+++ Colonizator/Broadcasters/MarketBroadcaster.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using System.Collections.Specialized;
+using Colonizator.Hubs;
+using GameLogic.Game;
+using GameLogic.Market;
+using Microsoft.AspNet.SignalR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +12,39 @@ namespace Colonizator.Broadcasters
 {
     public class MarketBroadcaster
     {
+        private static MarketBroadcaster _instance;
+        private IHubContext _context;
+        private Map _map;
+        private IMarket _market;
+
+        private MarketBroadcaster()
+        {
+            _context = GlobalHost.ConnectionManager.GetHubContext<MarketHub>();
+        }
+        public static MarketBroadcaster Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new MarketBroadcaster();
+                }
+                return _instance;
+            }
+        }
+
+        public void InitMarket(Map map)
+        {
+            foreach (var player in map.Players)
+            {
+                player.Resources.CollectionChanged += ResourcesOnCollectionChanged;
+                _context.Clients.Client(player.PlayerId).playerOrder(_market.GetOrders(player));
+            }
+        }
+
+        private void ResourcesOnCollectionChanged(object sender, NotifyCollectionChangedEventArgs notifyCollectionChangedEventArgs)
+        {
+
+        }
     }
 }
--- GameLogic/Game/Map.cs
+++ GameLogic/Game/Map.cs
@@ -8,11 +8,7 @@ namespace GameLogic.Game
 {
     public class Map
     {
-        public event ResourceUpdate ResourceUpdate;
-
         public string Id { get; set; }
         public List<Player> Players { get; set; }
     }
-
-    public delegate void ResourceUpdate(object sender, ResourceUpdateArgs args);
 }
